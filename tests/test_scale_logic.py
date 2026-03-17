4358acd9b8d7ac68b1852f2fccdc925df47092ca
Ray <mccormickraymond39@gmail.com>
2026-03-16 19:53:45 -0500

    Added two dictionaries of notes

---
+++ tests/test_scale_logic.py
@@ -0,0 +1,10 @@
+from Sight_Reading_Project.music_logic import generate_major_scale
+
+def test():
+    correct_scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
+    function_call_scale = generate_major_scale('C')
+    print(f"Correct scale is {correct_scale}")
+    print(f"Function call result: {function_call_scale}")
+
+if __name__ == "__main__":
+    test()
