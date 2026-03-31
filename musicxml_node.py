0a121037742e2b87526f67a81003d0f58642aed7
Ray <mccormickraymond39@gmail.com>
2026-03-31 16:04:47 -0500

    Added get_alter_value function

--- musicxml_node.py
+++ musicxml_node.py
@@ -1,3 +1,10 @@
 from constants import musicxml_head
 
-def create_musicxml_node()
+def get_alter_value(note_name):
+    if '#' in note_name:
+        return 1
+    elif 'b' in note_name:
+        return -1
+    return None
+
+print(get_alter_value('Dx'))
