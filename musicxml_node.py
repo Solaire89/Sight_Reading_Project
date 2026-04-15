6e09f13a2bbb0aef240ddb2eb719359960354791
Ray <mccormickraymond39@gmail.com>
2026-04-15 16:40:07 -0500

    Moved generate_note and create_attributes to musicxml_node.py and added a test. Also created create_measure function

--- musicxml_node.py
+++ musicxml_node.py
@@ -7,4 +7,32 @@ def get_alter_value(note_name):
         return -1
     return None
 
-print(get_alter_value('Dx'))
+# Note contents: <pitch> (<step> and <octave> within pitch), alter (flat or sharp),
+# duration (length of note in numbers), note type (quarter, half, whole, etc)
+def generate_note(step, octave, duration, note_type, alter=None):
+    if alter is not None:
+        note = f'<note><pitch><step>{step}</step><alter>{alter}</alter><octave>{octave}</octave></pitch>'
+    else:
+        note = f'<note><pitch><step>{step}</step><octave>{octave}</octave></pitch>'
+    note += f'<duration>{duration}</duration><type>{note_type}</type></note>'
+    return note
+
+# This is the overall form of the music. Includes elements like key, clef, and time signature
+def create_attributes(divisions, fifths, beats, beat_type, sign, clef_line):
+    return f'''<attributes><divisions>{divisions}</divisions>
+    <key><fifths>{fifths}</fifths></key>
+    <time><beats>{beats}</beats><beat-type>{beat_type}</beat-type></time>
+    <clef><sign>{sign}</sign><line>{clef_line}</line></clef></attributes>'''
+
+
+
+def create_measure(notes, measure_number, attributes=None):
+    if attributes:
+        measure = f"<measure number={measure_number}>" + attributes + notes + "</measure>"
+    else:
+        measure = f"<measure number={measure_number}>{notes}</measure>"
+    return measure
+
+attribute = create_attributes(4, 0, 4, 4, "g", 2)
+note = generate_note("C", 4, 4, "whole",)
+print(create_measure(note, 1, attribute))
